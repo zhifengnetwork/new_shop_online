6261dff87d803280d956f93eb9950493b13f8b4f
dengjiangg <2856986090@qq.com>
2019-06-20 11:35:59 +0800

    yongjin

--- application/admin/controller/Vip.php
+++ application/admin/controller/Vip.php
@@ -113,6 +113,91 @@ class Vip extends Base
         return $this->fetch();
     }
 
+    
+    /**
+     * 销售日志
+     * @return mixed
+     */
+    public function sales_log(){
+        $Ad = M('buy_vip');
+        $p = input('p/d');
+
+        $type = input('type',0);
+        $ctime = urldecode(I('ctime'));
+        $user_name = urldecode(I('user_name'));
+        $order_sn = I('order_sn');
+        $user_type = I('user_type');
+        $start_time = I('start_time');
+        $end_time = I('end_time');
+        // var_dump($type);die;
+        $where = [];
+        $log_ids = '';
+        $where = get_comm_condition($type); //获取条件
+        if($user_name){
+            $user['nickname'] = ['like', "%$user_name%"];
+            $id_list = M('users')->where($user)->column('user_id');
+
+            switch($user_type){
+                case 1:
+                    $where['user_id'] = [['in',$id_list],['neq',0],'and'];
+                    break;
+                case 2:
+                    $where['to_user_id'] = [['in',$id_list],['neq',0],'and'];
+                    break;
+                default: break;
+            }
+        }
+        if($ctime){
+            $gap = explode(' - ', $ctime);
+            $where['ctime'] = [['>= time',strtotime($start_time)],['< time',strtotime($end_time." 23:59:59")],'and'];
+        }
+        if ($order_sn) {
+            $where['order_sn'] = ['like',"%$order_sn%"];
+        }
+
+        $res = $Ad->where($where)->order('ctime','desc')->page($p . ',20')->select();
+
+        if ($res) {
+            foreach ($res as $val) {
+                $id_lists[] = $val['log_id'];
+                $list[] = $val;
+            }
+            $log_ids = implode(',',$id_lists);
+            $user_ids = array_column($list, 'user_id');
+            $to_user_ids = array_column($list, 'to_user_id');
+            $all_user_ids = array_merge($user_ids,$to_user_ids);
+            $all_user_name = M('users')->whereIn('user_id',$all_user_ids)->column('user_id,nickname,mobile');
+            $avatar = get_avatar($all_user_ids);
+
+            foreach ($list as $key => $value) {
+                $list[$key]['user_name'] = $all_user_name[$value['user_id']]['nickname'] ?: $all_user_name[$value['user_id']]['mobile'];
+                $list[$key]['to_user_name'] = $all_user_name[$value['to_user_id']]['nickname'] ?: $all_user_name[$value['to_user_id']]['mobile'];
+                $list[$key]['user_head_pic'] = $avatar[$value['user_id']];
+                $list[$key]['to_user_head_pic'] = $avatar[$value['to_user_id']];
+            }
+        }
+
+        $is_type = 6;
+
+        $this->assign('is_type',$is_type);
+        $this->assign('type',$type);
+        $this->assign('user_type',$user_type);
+        $this->assign('log_ids',urldecode($log_ids));
+        $this->assign('user_name',$user_name);
+        $this->assign('start_time',$start_time);
+        $this->assign('end_time',$end_time);
+        $this->assign('ctime',$gap[0].' - '.$gap[1]);
+        $this->assign('order_sn',$order_sn);
+        $this->assign('list', $list);
+        $count = $Ad->where($where)->count();
+        $Page = new Page($count, 20);
+        $show = $Page->show();
+        // dump($show);die;
+        $this->assign('count',$count);
+        $this->assign('page', $show);
+        return $this->fetch();
+    }
+
 
     //购买返佣
     public function export_commission_log()
